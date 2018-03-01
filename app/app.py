c6e7805dc6b2c035ab31282c6971ecfe49e481f5
Mikhail Logachev <miha.loga@gmail.com>
2018-03-01 14:20:12 +0300

    login and signup pages

--- app/app.py
+++ app/app.py
@@ -7,6 +7,21 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/stud/login')
+def stud_login():
+    return render_template('login.html', page_title="Student Login")
+
+@app.route('/stud/signup')
+def stud_signup():
+    return render_template('signup.html', page_title="Student Sign Up")
+
+@app.route('/empl/login')
+def empl_login():
+    return render_template('login.html', page_title="Employer Login")
+
+@app.route('/empl/signup')
+def empl_signup():
+    return render_template('signup.html', page_title="Employer Sign Up")
 
 if __name__ == '__main__':
     app.run()
