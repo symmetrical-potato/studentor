63186e2ddf543a5cf29c77cc331fcc088869a3f3
Mikhail Logachev <miha.loga@gmail.com>
2018-03-01 16:57:35 +0300

    action for templates

--- app/routes.py
+++ app/routes.py
@@ -8,11 +8,11 @@ def hello_world():
 
 @app.route('/stud/login', methods=['GET'])
 def get_stud_login():
-    return render_template('login.html', page_title="Student Login")
+    return render_template('login.html', page_title="Student Login", action='stud')
 
 @app.route('/stud/signup', methods=['GET'])
 def get_stud_signup():
-    return render_template('signup.html', page_title="Student Sign Up")
+    return render_template('signup.html', page_title="Student Sign Up", action='stud')
 
 @app.route('/stud/login', methods=['POST'])
 def login_student():
@@ -22,11 +22,11 @@ def login_student():
 
 @app.route('/empl/login', methods=['GET'])
 def get_empl_login():
-    return render_template('login.html', page_title="Employer Login")
+    return render_template('login.html', page_title="Employer Login", action='empl')
 
 @app.route('/empl/signup', methods=['GET'])
 def get_empl_signup():
-    return render_template('signup.html', page_title="Employer Sign Up")
+    return render_template('signup.html', page_title="Employer Sign Up", action='empl')
 
 @app.route('/empl/login', methods=['POST'])
 def login_empl():
