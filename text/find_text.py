d40de54f1738199c8f2d7f82966a7ef50553e010
Churikov Nikita <nikita@dri-lab.digdes.com>
2018-03-02 02:42:29 +0300

    add search by thesis

--- text/find_text.py
+++ text/find_text.py
@@ -70,7 +70,7 @@ def find_by_supervisor(supervisor):
     
     res = []
     
-    cur = es.search(index='uni', doc_type='СПбГУ', body=quer)
+    cur = es.search(index='uni', doc_type='diploma', body=quer)
     for hit in cur['hits']['hits']:
         res.append({'student_id'    : int(hit['_id']),
                     'student_name'  : st_names.name[int(hit['_id'])],
@@ -93,7 +93,28 @@ def find_students_by_theme(theme_name):
                 }, ...
             ]
     '''
-    pass
+    
+    quer = { 
+        'query':  
+        {
+            'match' : 
+            {
+                'text' : theme_name,
+            }
+        }
+    }
+    res = []
+    
+    cur = es.search(index='uni', doc_type='diploma', body=quer)
+    for hit in cur['hits']['hits']:
+        res.append({'id'    : int(hit['_id']),
+                   'score'  : hit['_score'],
+                   'title'  : hit['_source']['title'],
+                   'link'   : hit['_source']['link'],
+                   'type'   : 'diploma'})
+        
+        
+    return res
 
 
 def find_vacancy_by_student(student_name):
