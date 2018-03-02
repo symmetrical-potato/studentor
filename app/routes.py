0c6af3afff3fd1f9e1d5fafd8a8730ce385f5e29
Artyom Goncharov <artyom.goncharov1@gmail.com>
2018-03-02 04:59:37 +0300

    recommended students

--- app/routes.py
+++ app/routes.py
@@ -220,11 +220,20 @@ def get_event(empl_id, id):
             ("Name 1", 1),
             ("Name 2", 2),
         ]
-    recommended_students = [
-            ("Name Lastname", 3, 4.2),
-            ("Another Nameless", 4, 4.9),
-            ("Some RandomGuy", 5, 1)
-        ]
+
+    query = event.name + " " + event.description
+    res = find_text.find_students_by_theme(query)
+
+    def enrich_response(record):
+        student_id = record['id']
+        student = Student.query.filter_by(id=student_id).first()
+
+        if student is None:
+            return {}
+
+        return student.name, student.id, record['score']
+
+    recommended_students = list(map(enrich_response, res))
 
     if current_user.id == empl_id:
         is_owner = True
@@ -285,7 +294,7 @@ def search():
     return render_template('search.html')
 
 @app.route('/search/api', methods=['GET'])
-def search_api():
+def search_api__():
     query = request.args.get('q')
     res = find_text.find_by_string(query)
 
