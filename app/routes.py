d16c7c4b405a06e8ea7eb519ea2527e170dd9493
sergey <stalker-fc@yandex.ru>
2018-03-02 08:17:45 +0300

    fix

--- app/routes.py
+++ app/routes.py
@@ -232,7 +232,7 @@ def get_event(empl_id, id):
             ("Name 2", 2),
         ]
 
-    query = str('{} {}'.format(event.name, event.description), 'utf-8')
+    query = '{} {}'.format(event.name, event.description)
     print(query)
     res = find_text.find_students_by_theme(query)
     print(res)
