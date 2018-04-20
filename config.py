bc6777a26cb205348a230d321c29cdde3d249df7
Artyom Goncharov <artyom.goncharov1@gmail.com>
2018-04-20 16:49:26 +0300

    Изменение конфигурационного файла.

--- config.py
+++ config.py
@@ -4,5 +4,22 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Lm14vZ@localhost/studentor?client_encoding=utf8'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Lm14vZ@localhost/studentor?client_encoding' \
+                              '=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    DATA_LOADER_PERIOD = 10 #seconds
+
+    VK_API_KEY = os.environ.get('VK_API_KEY')
+
+    PATH_TO_CACHE_DIR = os.path.join(basedir, ".cache")
+    PATH_TO_LOG_DIR = os.path.join(basedir, ".log")
+    
+    PATH_TO_DATA_LOADER_CACHE = os.path.join(PATH_TO_CACHE_DIR, "data_loader_cache.txt")
+    PATH_TO_DATA_LOADER_LOG = os.path.join(PATH_TO_LOG_DIR, "data_loader_log.txt")
+
+if not os.path.isdir(Config.PATH_TO_CACHE_DIR):
+    os.mkdir(Config.PATH_TO_CACHE_DIR)
+
+if not os.path.isdir(Config.PATH_TO_LOG_DIR):
+    os.mkdir(Config.PATH_TO_LOG_DIR)
