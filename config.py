2ecc2cd0e088fdea4b46ff9831caf1e0fa14d261
Artyom Goncharov <artyom.goncharov1@gmail.com>
2018-04-23 19:11:08 +0300

    Изменен config файл.

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 import os
+
+from pathlib import Path
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    HOME = str(Path.home())
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Lm14vZ@localhost/studentor?client_encoding' \
+    SQLALCHEMY_DATABASE_URI = 'postgresql://artyom:ilavah84@localhost/studentor?client_encoding' \
                               '=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -12,16 +17,13 @@ class Config(object):
 
     VK_API_KEY = os.environ.get('VK_API_KEY')
 
-    PATH_TO_CACHE_DIR = os.path.join(basedir, ".cache")
-    PATH_TO_LOG_DIR = os.path.join(basedir, ".log")
+    PATH_TO_CACHE_DIR = os.path.join(HOME, ".cache/studentor")
+    PATH_TO_LOG_DIR = os.path.join(PATH_TO_CACHE_DIR, "log")
     
     PATH_TO_DATA_LOADER_CACHE = os.path.join(PATH_TO_CACHE_DIR, "data_loader_cache.txt")
     PATH_TO_DATA_LOADER_LOG = os.path.join(PATH_TO_LOG_DIR, "data_loader_log.txt")
-    UPLOAD_FOLDER = '/home/merlin/.cache/studentor/diplomas/'
-    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    UPLOAD_FOLDER = os.path.join(PATH_TO_CACHE_DIR, 'diplomas')
 
-if not os.path.isdir(Config.PATH_TO_CACHE_DIR):
-    os.mkdir(Config.PATH_TO_CACHE_DIR)
-
-if not os.path.isdir(Config.PATH_TO_LOG_DIR):
-    os.mkdir(Config.PATH_TO_LOG_DIR)
+os.makedirs(Config.UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(Config.PATH_TO_CACHE_DIR, exist_ok=True)
+os.makedirs(Config.PATH_TO_LOG_DIR, exist_ok=True)
